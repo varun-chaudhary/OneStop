bc042b27089e75ff23567fd00c9579f98f328a55
Varun Chaudhary <varunc105@gmail.com>
2024-04-08 00:20:42 +0530

    Authentication Done

---
+++ OneStop/app/src/main/java/com/example/onestop/LandingPage.kt
@@ -0,0 +1,91 @@
+package com.example.onestop
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.onestop.quicknotes.QuickNotes
+import com.example.onestop.reminder.ReminderFragment
+import com.example.onestop.taskmanager.TaskManager
+import com.google.android.material.navigation.NavigationView
+
+class LandingPage : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
+    lateinit var  drawer: DrawerLayout
+    lateinit var navigationView: NavigationView
+    lateinit var usertv:TextView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+        var navigationView = findViewById<NavigationView>(R.id.nav_view)
+        val headerView = navigationView.getHeaderView(0)
+        usertv = headerView.findViewById(R.id.navusername)
+
+        val username = intent.getStringExtra("username")
+        usertv.text = username
+
+        var toolbar: Toolbar
+                = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        var actionBarDrawerToggle = ActionBarDrawerToggle(this,drawer,toolbar,
+            R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+
+        drawer.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.reminder -> {
+                val reminderFragment = ReminderFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, reminderFragment)
+                    .commit()
+            }
+            R.id.task_manager -> {
+                val task_manager = TaskManager()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, task_manager)
+                    .commit()
+            }
+            R.id.notes -> {
+                val quick_notes = QuickNotes()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, quick_notes)
+                    .commit()
+            }
+//
+//            R.id.nav_chat -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,ChatFragment())
+//                .commit()
+//
+//            R.id.nav_profile -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,ProfileFragment())
+//                .commit()
+//
+//            R.id.nav_share -> Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show()
+//            R.id.nav_send -> Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show()
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+
+    }
+}
--- OneStop/app/src/main/java/com/example/onestop/MainActivity.kt
+++ OneStop/app/src/main/java/com/example/onestop/MainActivity.kt
@@ -1,83 +1,102 @@
 package com.example.onestop
 
-import android.os.Bundle
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import com.example.onestop.quicknotes.QuickNotes
-import com.example.onestop.reminder.ReminderFragment
-import com.example.onestop.taskmanager.TaskManager
-import com.google.android.material.navigation.NavigationView
-
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
-
-    lateinit var  drawer: DrawerLayout
-    lateinit var navigationView: NavigationView
-
+class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var pinEditText: EditText
+    private lateinit var usernameTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        var toolbar: Toolbar
-                = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val savedPin = sharedPreferences.getString("PIN", "")
 
-        drawer = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        if (savedPin.isNullOrEmpty()) {
+            // PIN not set, navigate to set login data activity
+            setContentView(R.layout.setlogindata)
 
-        var actionBarDrawerToggle = ActionBarDrawerToggle(this,drawer,toolbar,
-            R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+            val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+            pinEditText = findViewById(R.id.pinEditText)
+            val confirmPinEditText = findViewById<EditText>(R.id.confirmPinEditText)
+            val saveButton = findViewById<Button>(R.id.saveButton)
 
-        drawer.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
+            saveButton.setOnClickListener {
+                val username = usernameEditText.text.toString()
+                val pin = pinEditText.text.toString()
+                val confirmPin = confirmPinEditText.text.toString()
 
+                if (pin == confirmPin) {
+                    // Save username and PIN to SharedPreferences
+                    sharedPreferences.edit().apply {
+                        putString("Username", username)
+                        putString("PIN", pin)
+                        apply()
+                    }
 
-    }
+                    Toast.makeText(
+                        this,
+                        "Username and PIN saved",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startact(this,username)
 
-    override fun onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+                    finish()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "PINs do not match",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         } else {
-            super.onBackPressed();
+            // PIN already set, show login activity
+            setContentView(R.layout.login)
+
+            pinEditText = findViewById(R.id.pinEditText)
+            usernameTextView = findViewById(R.id.usernameTextView)
+            val enterPinButton = findViewById<Button>(R.id.enterPinButton)
+
+            // Set the username to the TextView
+            val savedUsername = sharedPreferences.getString("Username", "") ?: ""
+            usernameTextView.text = "Welcome, $savedUsername"
+
+            enterPinButton.setOnClickListener {
+                val enteredPin = pinEditText.text.toString()
+                val savedPin = sharedPreferences.getString("PIN", "")
+
+                if (enteredPin == savedPin) {
+                    // Correct PIN entered, proceed to landing page
+                    startact(this,savedUsername)
+                    finish()
+                } else {
+
+                    Toast.makeText(
+                        this,
+                        "Incorrect PIN",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId)
-        {
-            R.id.reminder -> {
-                val reminderFragment = ReminderFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, reminderFragment)
-                    .commit()
-            }
-            R.id.task_manager -> {
-                val task_manager = TaskManager()
-                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, task_manager)
-                    .commit()
-            }
-            R.id.notes -> {
-                val quick_notes = QuickNotes()
-                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, quick_notes)
-                    .commit()
-            }
-//
-//            R.id.nav_chat -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,ChatFragment())
-//                .commit()
-//
-//            R.id.nav_profile -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,ProfileFragment())
-//                .commit()
-//
-//            R.id.nav_share -> Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show()
-//            R.id.nav_send -> Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show()
-        }
-        drawer.closeDrawer(GravityCompat.START)
-        return true
-
+    fun startact(context: Context,name:String){
+        val intent = Intent(context
+            , LandingPage::class.java)
+        intent.putExtra("username",name)
+        startActivity(intent)
     }
 }
+
+
+
